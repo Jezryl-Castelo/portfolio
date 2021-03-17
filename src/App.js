f77720bb59d6d461abb41d0cfff9c71e8013d7f4
Jezryl <yamabags@gmail.com>
2021-03-17 14:56:33 -0600

    redeploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Nav from './components/Nav';
 //Pages
-import Home from './pages/home';
+import Home from './pages/Home';
 import Contact from './pages/Contact';
 import GlobalStyle from './components/GlobalStyle';
 import Footer from './components/Footer';
